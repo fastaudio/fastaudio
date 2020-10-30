00c6074f3253a593a8e483c9a766292902260fa2
Harry <himion0@gmail.com>
2020-10-30 11:43:15 +0000

    Using soudnfile torchaudio backend

--- src/fastaudio/__init__.py
+++ src/fastaudio/__init__.py
@@ -2,6 +2,7 @@
 from pkg_resources import DistributionNotFound, get_distribution
 import torchaudio
 
+torchaudio.USE_SOUNDFILE_LEGACY_INTERFACE = False
 torchaudio.set_audio_backend("soundfile")
 try:
     # Change here if project is renamed and does not equal the package name

5e18be48e6be9d69a1329d3caba21f8c627eb6b6
Harry Coultas Blum <himion0@gmail.com>
2020-11-13 13:00:20 +0000

    Added empty function to be called

--- src/fastaudio/ci.py
+++ src/fastaudio/ci.py
@@ -2,9 +2,13 @@ import os
 from functools import wraps
 
 
+def _empty():
+    return
+
+
 def skip_if_ci(func):
     @wraps(func)
-    def _inner(alt_func=lambda x: None):
+    def _inner(alt_func=_empty):
         if "CI" in os.environ and os.environ["CI"] == "true":
             return alt_func()
         return func()

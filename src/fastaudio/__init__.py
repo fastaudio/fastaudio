8538a19d872d99abd6db7d13314ec767416d64f6
Robert Bracco <robertbracco1@gmail.com>
2020-12-12 22:21:11 +0000

    change linux backend to fix mp3 load bug

--- src/fastaudio/__init__.py
+++ src/fastaudio/__init__.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
 from pkg_resources import DistributionNotFound, get_distribution
 
+import os
 import torchaudio
 
 torchaudio.USE_SOUNDFILE_LEGACY_INTERFACE = False
-torchaudio.set_audio_backend("soundfile")
+# soundfile is torchaudio backend for windows, all other os use sox_io
+backend = "soundfile" if os.name == "nt" else "sox_io"
+torchaudio.set_audio_backend(backend)
 try:
     # Change here if project is renamed and does not equal the package name
     dist_name = __name__
